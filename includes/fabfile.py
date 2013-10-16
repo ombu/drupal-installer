0817f64d78fc855550a1f2c5afce5667ed89aec6
Jon Duell <jon@jonduell.com>
2013-10-16 10:01:54 -0700

    Updates fabfile to match new puppet infrastructure

--- includes/fabfile.py
+++ includes/fabfile.py
@@ -4,6 +4,8 @@ from fabric.contrib import files
 from fabric.operations import run
 
 env.repo_type = 'git'
+env.repo_url = 'git@bitbucket.org:ombu/CHANGEME.git'
+env.use_ssh_config = 'true'
 
 # Global environment settings
 env.site_name = '__SITENAME__'
@@ -16,21 +18,28 @@ env.dev_modules = 'devel devel_generate context_ui views_ui diff'
 
 # Host settings
 @task
+def local():
+    """
+    The local host definition
+    """
+    env.db_user = 'phpuser'
+    env.db_pw = 'meh'
+    env.db_host = 'localhost'
+
 def qa():
     """
     The qa server definition
     """
     # TODO: fill out this info with the correct QA specs
-    # env.hosts = ['ombu@paco.ombuweb.com:34165']
+    # env.hosts = ['pepe.ombuweb.com']
     # env.host_type = 'qa'
-    # env.user = 'ombu'
     # env.host_webserver_user = 'www-data'
-    # env.host_site_path = '/vol/main/qa/qaX'
+    # env.host_site_path = '/var/www/qaX'
     # env.public_path = 'current'
-    # env.db_db = '__DBNAME__'
+    # env.db_db = 'qaX'
     # env.db_user = 'phpuser'
     # env.db_pw = 'meh'
-    # env.db_host = 'localhost'
+    # env.db_host = 'qadb.cpuj5trym3at.us-west-2.rds.amazonaws.com'
 
 @task
 def staging():
@@ -38,13 +47,12 @@ def staging():
     The staging server definition
     """
     # TODO: fill out this info with the correct Staging specs
-    # env.hosts = ['ombu@paco.ombuweb.com:34165']
+    # env.hosts = ['pepe.ombuweb.com']
     # env.host_type = 'staging'
-    # env.user = 'ombu'
     # env.host_webserver_user = 'www-data'
-    # env.host_site_path = '/vol/main/stage/stageX'
+    # env.host_site_path = '/var/www/stageX'
     # env.public_path = 'current'
-    # env.db_db = '__DBNAME___stage'
+    # env.db_db = 'stageX'
     # env.db_user = 'phpuser'
     # env.db_pw = 'meh'
-    # env.db_host = 'localhost'
+    # env.db_host = 'qadb.cpuj5trym3at.us-west-2.rds.amazonaws.com'
